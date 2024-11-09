992ae27923bb191d32c5dc06f875fcd06a6f6e24
vishwadwivedi35 <68500903+vishwadwivedi35@users.noreply.github.com>
2024-11-10 01:32:21 +0530

    order payload changes

--- server/models/order.models.js
+++ server/models/order.models.js
@@ -25,6 +25,10 @@ const orderSchema = new mongoose.Schema({
         ref: "Product",
         required: true,
       },
+      name: {
+        type: String,
+        required: true,
+      },
       quantity: {
         type: Number,
         required: true,
